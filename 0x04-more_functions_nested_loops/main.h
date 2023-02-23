762bc2b838ed02de85244f474604248cb6d0f1c5
khrispyshots <christianchuks3@gmail.com>
2023-02-23 03:16:13 -0800

    992

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -14,9 +13,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-
-#endif 
-/* 
-Bwave ICT 
-*/
+#endif /* MAIN_H */
